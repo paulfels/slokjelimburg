1191c3497fc5c5f569805dd4fb7436d44958009d
Paul Fels <paulfels@Pauls-MBP.fritz.box>
2019-05-21 10:12:44 +0200

    test2

--- js/map.js
+++ js/map.js
@@ -184,7 +184,7 @@ require([
                 });
 
                 //Need to add address bar height back to map
-                return screen.availHeight - window.innerHeight - 120;// - 40;
+                return screen.availHeight - window.innerHeight - 40;
                 /* 40 = height of bottom safari toolbar */
             }
             else { //landscape
