3709e86c429b53e1c5892e1c0cc3497648a9a588
Paul Fels <paulfels@Pauls-MBP.fritz.box>
2019-05-21 11:40:22 +0200

    Reposition border

--- js/map.js
+++ js/map.js
@@ -195,9 +195,8 @@ require([
                     resizeMap();
                 });
 
-                // dom.byId("divMap").style.position = "relative";
-                // dom.byId("divMap").style.top = 20 + "px";
-                // dom.byId("divMap").style.left = 20 + "px";
+                dom.byId("border").style.position = "relative";
+                dom.byId("border").style.top = 40 + "px";
                 return 0;                
             }
         }
